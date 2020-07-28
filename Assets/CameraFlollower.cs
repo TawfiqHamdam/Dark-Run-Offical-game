da1749d91642edc121c0973552f74d74a675f1e2
DESKTOP-F9FV4UU\LENOVO <tawfiqhamdam@gmail.com>
2020-07-28 13:45:50 -0400

    starting

---
+++ Assets/CameraFlollower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFlollower : MonoBehaviour
+{
+
+    [SerializeField] Transform Taget;
+    [SerializeField] Vector3 offset;
+
+    [SerializeField] float SmothTime = 0.25f;
+
+    void LateUpdate()
+    {
+        Vector3 WantPos = Taget.position + offset;
+        transform.position = WantPos;
+    }
+}
---
+++ Assets/_ScriptableObjects/List_GameObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DarkRun.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "List_GameObject",
+                     menuName = "CLYKI/ScriptableObject/List_GameObject")]
+    public class List_GameObject : ScriptableObject
+    {
+        [SerializeField] List<GameObject> value = new List<GameObject>();
+
+        public bool Contains(GameObject value) { return this.value.Contains(value); }
+        public int Count() { return value.Count; }
+        public GameObject Value(int index) { return value[index]; }
+
+#if UNITY_EDITOR
+        public void Add(GameObject value)
+        {
+            switch (value == null) { case true: return; }
+            switch (this.value.Contains(value)) { case true: return; }
+            this.value.Add(value);
+            CleanUp();
+        }
+        public void Remove(GameObject value)
+        {
+            switch (this.value.Contains(value)) { case false: return; }
+            this.value.Remove(value);
+            CleanUp();
+        }
+        private void CleanUp()
+        {
+            for (int i = 0; i < value.Count; i++)
+            {
+                switch (value[i] == null) { case false: continue; }
+                value.Remove(value[i]);
+            }
+        }
+        public void Scrub() { value = new List<GameObject>(); }
+#endif
+    }
+}
