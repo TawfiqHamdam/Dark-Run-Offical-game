7de34e3c04f1c28c86ebfa097ed90ad0607fd29a
lawson <lawsonadams12335@gmaail.com>
2020-08-24 09:30:21 -0400

    Photon
    photon merge test

---
+++ Assets/Photon/PhotonUnityNetworking/Resources/GameSetupController.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using System.IO;
+using UnityEngine;
+
+public class GameSetupController : MonoBehaviourPunCallbacks
+{
+    
+    void Start()
+    {
+        CreatePlayer(); // create networked player object for each player that loads into the multiplayer 
+    }
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+            
+    }
+    
+   
+}
