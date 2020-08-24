e7714c41f079bdd6cfdf4897c3178dac3820ab1f
lawson <lawsonadams12335@gmaail.com>
2020-08-24 13:11:37 -0400

    pjij

--- Assets/Photon/PhotonUnityNetworking/Resources/QuickStartLobbyController.cs
+++ Assets/Photon/PhotonUnityNetworking/Resources/QuickStartLobbyController.cs
@@ -8,22 +8,22 @@ using UnityEngine;
 public class QuickStartLobbyController : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    private GameObject _quickStartButton; //button used for creating a game
+    private GameObject quickStartButton; //button used for creating a game
     [SerializeField]
-    private GameObject _quickCancelButton; //button used to stop searching for game
+    private GameObject quickCancelButton; //button used to stop searching for game
     [SerializeField]
-    private int _roomSize = 10; // set this to max amount of people in the room at one time
+    private int roomSize = 10; // set this to max amount of people in the room at one time
 
     public override void OnConnectedToMaster()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
-        _quickStartButton.SetActive(true);
+        quickStartButton.SetActive(true);
     }
 
     public void QuickStart()
     {
-        _quickStartButton.SetActive(false);
-        _quickCancelButton.SetActive(true);
+        quickStartButton.SetActive(false);
+        quickCancelButton.SetActive(true);
         PhotonNetwork.JoinRandomRoom(); // first tries to join a random room
         Debug.Log("Quick Start");
     }
@@ -38,7 +38,7 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
     {
         Debug.Log("creating room now");
         int randomRoomNumber = Random.Range(0, 1000);
-        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)_roomSize };
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
         PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
         Debug.Log(randomRoomNumber);
     }
@@ -51,8 +51,8 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
     }
     public void QuickCancel()
     {
-        _quickCancelButton.SetActive(false);
-        _quickStartButton.SetActive(true);
+        quickCancelButton.SetActive(false);
+        quickStartButton.SetActive(true);
         PhotonNetwork.LeaveRoom();
 
     }
