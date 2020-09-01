7660e173ad9a07a0c9437fbecbe7e6819e740754
DESKTOP-F9FV4UU\LENOVO <tawfiqhamdam@gmail.com>
2020-08-31 23:51:12 -0400

    Securement mesure
    so more then 1 boss does not spawn

---
+++ Assets/Spawning.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    [SerializeField] GameObject zombie;
+    bool spawned = false;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (spawned == false)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                Instantiate(zombie);
+                spawned = true;
+            }
+        }
+    }
+}
